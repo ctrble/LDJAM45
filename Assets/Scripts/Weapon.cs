4ee8f543a16871bab306c3910396027cc32eaa97
Charlotte Trible <ctrble@gmail.com>
2019-10-05 09:06:54 -0400

    adding crosshairs and attack rays

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -7,9 +7,20 @@ public class Weapon : MonoBehaviour {
   private Weapon_Data weaponData;
   [SerializeField]
   private Game_Event OnWeaponSelected;
+  public LayerMask hitLayer;
+  private Camera mainCamera;
+  public Transform crosshairs;
 
   void OnEnable() {
     SelectWeapon();
+
+    if (mainCamera == null) {
+      mainCamera = Camera.main;
+    }
+
+    if (crosshairs == null) {
+      crosshairs = GameObject.FindGameObjectWithTag("Crosshairs").transform;
+    }
   }
 
   public void SelectWeapon() {
@@ -17,9 +28,22 @@ public class Weapon : MonoBehaviour {
   }
 
   public void UseWeapon() {
-    Debug.Log(weaponData.WeaponName);
-    Debug.Log(weaponData.Icon.name);
-    Debug.Log(weaponData.AttackDamage);
-    Debug.Log(weaponData.AttackRange);
+    // Debug.Log(weaponData.WeaponName);
+    // Debug.Log(weaponData.Icon.name);
+    // Debug.Log(weaponData.AttackDamage);
+    // Debug.Log(weaponData.AttackRange);
+
+    RaycastHit hit;
+    Vector3 attackPosition = mainCamera.transform.position;
+    Vector3 attackDirection = crosshairs.position - mainCamera.transform.position;
+
+    bool hitCast = Physics.Raycast(attackPosition, attackDirection, out hit, weaponData.AttackRange, hitLayer);
+    if (hitCast) {
+      Debug.Log(hit.transform.name);
+      Debug.DrawRay(attackPosition, attackDirection * weaponData.AttackRange, Color.red);
+    }
+    else {
+      Debug.DrawRay(attackPosition, attackDirection * weaponData.AttackRange, Color.black);
+    }
   }
 }
