4df30cb5ae3d321aadb19c2171720453e97ff7e4
Charlotte Trible <ctrble@gmail.com>
2019-10-06 15:06:53 -0400

    adding angles to enemy attack decisions

--- Assets/Scripts/Enemy_Actions.cs
+++ Assets/Scripts/Enemy_Actions.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class Enemy_Actions : MonoBehaviour {
   public GameObject inventoryObject;
   public Entity enemyEntity;
+  public Transform player;
   private int currentItem;
   public List<GameObject> heldItems;
   [SerializeField]
   private bool primaryAction;
+  public float angleFromPlayer = 0;
+  public float attackAngleThreshold = 20f;
 
   void Start() {
     if (enemyEntity == null) {
       enemyEntity = gameObject.GetComponent<Entity>();
+    }
+
+    if (player == null) {
+      player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     primaryAction = false;
@@ -27,6 +34,17 @@ public class Enemy_Actions : MonoBehaviour {
     if (heldItems.Count != inventoryObject.transform.childCount || !heldItems[currentItem].activeInHierarchy) {
       GetHeldItems();
       ChangeItem();
+    }
+
+    Vector3 playerDirection = player.position - transform.position;
+    angleFromPlayer = Vector3.Angle(playerDirection, transform.forward);
+
+    // close enough to attack?
+    if (angleFromPlayer <= attackAngleThreshold) {
+      primaryAction = true;
+    }
+    else {
+      primaryAction = false;
     }
 
     if (primaryAction) {
